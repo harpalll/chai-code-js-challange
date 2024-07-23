5a13e5dd5e3b34291709bee974abcb68d1ad5e9c
harpalll <sindhavharpalsinh62@gmail.com>
2024-07-23 19:17:46 +0530

    day 6 : arrays completed.

---
+++ day6/arrays.js
@@ -0,0 +1,35 @@
+// array creation and access
+const nums = [1, 2, 3, 4, 5]
+// console.log(nums)   
+// console.log(nums[0])   
+// console.log(nums[nums.length-1])   
+
+// array methods (basic)
+// nums.push(5)
+// nums.pop()
+// nums.shift()
+// nums.unshift(5)
+
+// array methods (intermediate)
+
+// const doubledElems = nums.map((num) => num*2)
+// const onlyEvenElems = nums.filter((num) => num % 2 === 0)
+// const totalOfElems = nums.reduce((acc, curr) => acc += curr, 0)
+// console.log(totalOfElems)   
+
+// array iteration
+// for (let index = 0; index < nums.length; index++) {
+//     const element = nums[index];
+//     console.log(element);
+// }
+// nums.forEach((num) => console.log(num))
+
+// multi-dimensional arrays
+// create a two dimensional array(matrix) and log the entire array to console.
+const matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+// console.log(matrix) 
+// console.log(matrix[0][1]) 
